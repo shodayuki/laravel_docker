10e50ebf1f3c8dec4c858abab75e03d4605c0f63
shodayuki <yshoda1227@gmail.com>
2023-01-07 09:19:46 +0900

    シーダークラスを作成 && 日本語文字化け調整

---
+++ database/seeders/AuthorsTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class AuthorsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for ($i = 1; $i <= 10; $i++) {
+            $author = [
+                'name' => '著者名' . $i,
+                'kana' => 'チョシャメイ' . $i,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ];
+            \Illuminate\Support\Facades\DB::table('authors')->insert($author);
+        }
+    }
+}
