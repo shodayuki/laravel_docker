1713228103f0bb933ed0d15388b8583c15361811
shodayuki <yshoda1227@gmail.com>
2023-01-08 17:20:51 +0900

    API作成

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('/publishers', [App\Http\Controllers\PublisherAction::class, 'create']);
